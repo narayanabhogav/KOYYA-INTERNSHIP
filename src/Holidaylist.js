486daccec91aa631682eaac491b8432775d87259
narayanabhogav <147239961+narayanabhogav@users.noreply.github.com>
2024-09-12 11:43:01 +0530

    Update Holidaylist.js

--- src/Holidaylist.js
+++ src/Holidaylist.js
@@ -1,6 +1,31 @@
 import React from "react";
 import Head from './head.png';
 import {Helmet} from 'react-helmet';
+
+const holidays = [
+  { id: 1, month: 'April', date: 9, day: 'Tuesday', occasion: 'Ugadi' },
+  { id: 2, month: 'April', date: 10, day: 'Wednesday', occasion: 'Ramadan' },
+  { id: 3, month: 'April', date: 14, day: 'Sunday', occasion: 'Ambedkar Jayanthi', light: true },
+  { id: 4, month: 'May', date: 1, day: 'Wednesday', occasion: 'May Day' },
+  { id: 5, month: 'August', date: 15, day: 'Thursday', occasion: 'Independence Day' },
+  { id: 6, month: 'September', date: 7, day: 'Saturday', occasion: 'Vinayaka Chavithi', light: true },
+  { id: 7, month: 'October', date: 12, day: 'Saturday', occasion: 'Mahanavami', light: true },
+  { id: 8, month: 'October', date: 13, day: 'Sunday', occasion: 'Vijayadashami', light: true },
+  { id: 9, month: 'October', date: 2, day: 'Wednesday', occasion: 'Gandhi Jayanthi' },
+  { id: 10, month: 'October', date: 30, day: 'Wednesday', occasion: 'Naraka Chathurdhashi' },
+  { id: 11, month: 'October', date: 31, day: 'Thursday', occasion: 'Deepavali' },
+  { id: 12, month: 'November', date: 1, day: 'Friday', occasion: 'Kannada Rajyotsava' },
+  { id: 13, month: 'December', date: 25, day: 'Wednesday', occasion: 'Christmas' },
+  { id: 14, month: 'January', date: 1, day: 'Wednesday', occasion: 'New Year Day' },
+  { id: 15, month: 'January', date: 13, day: 'Monday', occasion: 'Bhogi' },
+  { id: 16, month: 'January', date: 14, day: 'Tuesday', occasion: 'Makarasankranthi' },
+  { id: 17, month: 'January', date: 15, day: 'Wednesday', occasion: 'Kanuma' },
+  { id: 18, month: 'January', date: 26, day: 'Sunday', occasion: 'Republic Day', light: true },
+  { id: 19, month: 'February', date: 26, day: 'Wednesday', occasion: 'Mahashivarathri' },
+  { id: 20, month: 'March', date: 30, day: 'Sunday', occasion: 'Ugadi', light: true },
+  { id: 21, month: 'March', date: 31, day: 'Monday', occasion: 'Ramadan' },
+];
+
 function Holidaylist() {
   return (
     <div className="max-w-4xl mx-auto p-8 pb-32">
@@ -15,166 +40,29 @@ function Holidaylist() {
         <p className="text-2xl font-bold">2024-25</p>
       </header>
   
-      <table className="font-semibold min-w-full border-collapse border border-gray-300">
-        <thead>
-          <tr >
-            <th className="border border-gray-300 p-2">#</th>
-            <th className="border border-gray-300 p-2">Month</th>
-            <th className="border border-gray-300 p-2">Date</th>
-            <th className="border border-gray-300 p-2">Day</th>
-            <th className="border border-gray-300 p-2">Occasion</th>
+      <table className="font-semibold min-w-full border-collapse border border-gray-300 w-full">
+      <thead>
+        <tr>
+          <th className="border border-gray-300 p-2">#</th>
+          <th className="border border-gray-300 p-2">Month</th>
+          <th className="border border-gray-300 p-2">Date</th>
+          <th className="border border-gray-300 p-2">Day</th>
+          <th className="border border-gray-300 p-2">Occasion</th>
+        </tr>
+      </thead>
+      <tbody>
+        {holidays.map((holiday) => (
+          <tr key={holiday.id} className={holiday.light ? 'font-light' : ''}>
+            <td className="border border-gray-300 p-2">{holiday.id}</td>
+            <td className="border border-gray-300 p-2">{holiday.month}</td>
+            <td className="border border-gray-300 p-2">{holiday.date}</td>
+            <td className="border border-gray-300 p-2">{holiday.day}</td>
+            <td className="border border-gray-300 p-2">{holiday.occasion}</td>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="border border-gray-300 p-2">1</td>
-            <td className="border border-gray-300 p-2">April</td>
-            <td className="border border-gray-300 p-2">9</td>
-            <td className="border border-gray-300 p-2">Tuesday</td>
-            <td className="border border-gray-300 p-2">Ugadi</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">2</td>
-            <td className="border border-gray-300 p-2">April</td>
-            <td className="border border-gray-300 p-2">10</td>
-            <td className="border border-gray-300 p-2">Wednesday</td>
-            <td className="border border-gray-300 p-2">Ramadan</td>
-          </tr>
-          <tr className="font-light">
-            <td className="border border-gray-300 p-2">3</td>
-            <td className="border border-gray-300 p-2">April</td>
-            <td className="border border-gray-300 p-2">14</td>
-            <td className="border border-gray-300 p-2">Sunday</td>
-            <td className="border border-gray-300 p-2">Ambedkar Jayanthi</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">4</td>
-            <td className="border border-gray-300 p-2">May</td>
-            <td className="border border-gray-300 p-2">1</td>
-            <td className="border border-gray-300 p-2">Wednesday</td>
-            <td className="border border-gray-300 p-2">May Day</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">5</td>
-            <td className="border border-gray-300 p-2">August</td>
-            <td className="border border-gray-300 p-2">15</td>
-            <td className="border border-gray-300 p-2">Thursday</td>
-            <td className="border border-gray-300 p-2">Independence Day</td>
-          </tr>
-          <tr className="font-light">
-            <td className="border border-gray-300 p-2">6</td>
-            <td className="border border-gray-300 p-2">September</td>
-            <td className="border border-gray-300 p-2">7</td>
-            <td className="border border-gray-300 p-2">Saturday</td>
-            <td className="border border-gray-300 p-2">Vinayaka Chavithi</td>
-          </tr>
-          <tr className="font-light">
-            <td className="border border-gray-300 p-2">7</td>
-            <td className="border border-gray-300 p-2">October</td>
-            <td className="border border-gray-300 p-2">12</td>
-            <td className="border border-gray-300 p-2">Saturday</td>
-            <td className="border border-gray-300 p-2">Mahanavami</td>
-          </tr>
-          <tr className="font-light">
-            <td className="border border-gray-300 p-2">8</td>
-            <td className="border border-gray-300 p-2">October</td>
-            <td className="border border-gray-300 p-2">13</td>
-            <td className="border border-gray-300 p-2">Sunday</td>
-            <td className="border border-gray-300 p-2">Vijayadashami</td>
-          </tr>
-           <tr>
-            <td className="border border-gray-300 p-2">9</td>
-            <td className="border border-gray-300 p-2">October</td>
-            <td className="border border-gray-300 p-2">2</td>
-            <td className="border border-gray-300 p-2">Wednesday</td>
-            <td className="border border-gray-300 p-2">Gandhi Jayanthi</td>
-          </tr> 
-          <tr>
-            <td className="border border-gray-300 p-2">10</td>
-            <td className="border border-gray-300 p-2">October</td>
-            <td className="border border-gray-300 p-2">30</td>
-            <td className="border border-gray-300 p-2">Wednesday</td>
-            <td className="border border-gray-300 p-2">Naraka Chathurdhashi</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">11</td>
-            <td className="border border-gray-300 p-2">October</td>
-            <td className="border border-gray-300 p-2">31</td>
-            <td className="border border-gray-300 p-2">Thursday</td>
-            <td className="border border-gray-300 p-2">Deepavali</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">12</td>
-            <td className="border border-gray-300 p-2">November</td>
-            <td className="border border-gray-300 p-2">1</td>
-            <td className="border border-gray-300 p-2">Friday</td>
-            <td className="border border-gray-300 p-2">Kannada Rajyotsava</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">13</td>
-            <td className="border border-gray-300 p-2">December</td>
-            <td className="border border-gray-300 p-2">25</td>
-            <td className="border border-gray-300 p-2">Wednesday</td>
-            <td className="border border-gray-300 p-2">Christmas</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">14</td>
-            <td className="border border-gray-300 p-2">January</td>
-            <td className="border border-gray-300 p-2">1</td>
-            <td className="border border-gray-300 p-2">Wednesday</td>
-            <td className="border border-gray-300 p-2">New Year Day</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">15</td>
-            <td className="border border-gray-300 p-2">January</td>
-            <td className="border border-gray-300 p-2">13</td>
-            <td className="border border-gray-300 p-2">Monday</td>
-            <td className="border border-gray-300 p-2">Bhogi</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">16</td>
-            <td className="border border-gray-300 p-2">January</td>
-            <td className="border border-gray-300 p-2">14</td>
-            <td className="border border-gray-300 p-2">Tuesday</td>
-            <td className="border border-gray-300 p-2">Makarasankranthi</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">17</td>
-            <td className="border border-gray-300 p-2">January</td>
-            <td className="border border-gray-300 p-2">15</td>
-            <td className="border border-gray-300 p-2">Wednesday</td>
-            <td className="border border-gray-300 p-2">Kanuma</td>
-          </tr>
-          <tr className="font-light">
-            <td className="border border-gray-300 p-2">18</td>
-            <td className="border border-gray-300 p-2">January</td>
-            <td className="border border-gray-300 p-2">26</td>
-            <td className="border border-gray-300 p-2">Sunday</td>
-            <td className="border border-gray-300 p-2">Republic Day</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">19</td>
-            <td className="border border-gray-300 p-2">February</td>
-            <td className="border border-gray-300 p-2">26</td>
-            <td className="border border-gray-300 p-2">Wednesday</td>
-            <td className="border border-gray-300 p-2">Mahashivarathri</td>
-          </tr>
-          <tr className="font-light">
-            <td className="border border-gray-300 p-2">20</td>
-            <td className="border border-gray-300 p-2">March</td>
-            <td className="border border-gray-300 p-2">30</td>
-            <td className="border border-gray-300 p-2">Sunday</td>
-            <td className="border border-gray-300 p-2">Ugadi</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 p-2">21</td>
-            <td className="border border-gray-300 p-2">March</td>
-            <td className="border border-gray-300 p-2">31</td>
-            <td className="border border-gray-300 p-2">Monday</td>
-            <td className="border border-gray-300 p-2">Ramadan</td>
-          </tr>
-        </tbody>
-      </table>
+        ))}
+      </tbody>
+    </table>
+
       <p className="font-bold text-xl mt-2 ">Total Number of Holidays: 21</p>
         <p className="font-bold text-xl ">Total Number of Effective Holidays: 15</p>
     </div>
